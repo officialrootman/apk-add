89a6073f23611d616ff65657357d33670aa3ad05
officialrootman <muhammetcanaltan41@gmail.com>
2025-04-04 21:26:50 +0300

    Update apk-add.py

--- apk-add.py
+++ apk-add.py
@@ -1,5 +1,31 @@
 import os
 
-os.system("apk add python")
-os.system("apk add python2")
-os.system("apk add python3")
+packages = [
+    "python",
+    "python2",
+    "python3",
+    "git",
+    "curl",
+    "vim",
+    "nano",
+    "htop",
+    "wget",
+    "bash",
+    "openssh",
+    "zip",
+    "unzip",
+    "make",
+    "gcc",
+    "g++",
+    "libc-dev",
+    "linux-headers",
+    "ca-certificates",
+    "openssl",
+    "perl",
+    "ruby",
+    "nodejs",
+    "npm"
+]
+
+for package in packages:
+    os.system(f"apk add {package}")
